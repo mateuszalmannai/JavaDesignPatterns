5b64c5192b7dee1c7c0d4c759eebaf4c2de3102d
mateusz <mateuszalmannai@googlemail.com>
2015-12-15 21:27:14 +0100

    Add state pattern example

---
+++ src/behavioral/state/balzholczer/Context.java
@@ -0,0 +1,13 @@
+package behavioral.state.balzholczer;
+
+public class Context {
+  private State state;
+
+  public State getState() {
+    return state;
+  }
+
+  public void setState(State state) {
+    this.state = state;
+  }
+}
---
+++ src/behavioral/state/balzholczer/State.java
@@ -0,0 +1,5 @@
+package behavioral.state.balzholczer;
+
+public interface State {
+  void doAction(Context context);
+}
